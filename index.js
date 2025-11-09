19fada9e41b5780f19196b970338ba2e254904aa
behzod07-abdullayev <bekzod2366@gmail.com>
2025-11-09 18:09:18 +0500

    3-oy-10-dars

--- index.js
+++ index.js
@@ -1,51 +1,117 @@
+const postsContainer = document.getElementById("postsContainer")
+const commentsContainer = document.getElementById("commentsContainer")
+const albumsContainer = document.getElementById("albumsContainer")
+const photosContainer = document.getElementById("photosContainer")
+const todosContainer = document.getElementById("todosContainer")
+const usersContainer = document.getElementById("usersContainer")
 
-const usersContainer = document.getElementById("usersContainer");
-const todosContainer = document.getElementById("todosContainer");
-const albumsContainer = document.getElementById("albumsContainer");
+async function myFunc() {
+    const post = await fetch ("https://jsonplaceholder.typicode.com/posts")
+    const comment = await fetch ("https://jsonplaceholder.typicode.com/comments")
+    const album = await fetch ("https://jsonplaceholder.typicode.com/albums")
+    const photo = await fetch ("https://jsonplaceholder.typicode.com/photos")
+    const todo = await fetch ("https://jsonplaceholder.typicode.com/todos")
+    const user = await fetch ("https://jsonplaceholder.typicode.com/users")
 
-async function myDatas() {
-  const usersRes = await fetch("https://jsonplaceholder.typicode.com/users");
-  const todosRes = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const albumsRes = await fetch("https://jsonplaceholder.typicode.com/albums");
 
-  const users = await usersRes.json();
-  const todos = await todosRes.json();
-  const albums = await albumsRes.json();
 
-  users.forEach((user, i) => {
-    const card = document.createElement("div");
-    card.className = "card";
+    const post1 = await post.json()
+    const comment1 = await comment.json()
+    const album1 = await album.json()
+    const photo1 = await photo.json()
+    const todo1 = await todo.json()
+    const user1 = await user.json()
+
+
+
+    post1.forEach((post, i) => {
+        const card = document.createElement("div")
+        card.className = "cardPost"
+        card.innerHTML = `
+        <img src = "https://picsum.photos/200?random=${i}">
+        <p><strong>${post.userId}</strong></p>
+        <p>${post.title}</p>
+        <p>${post.body}</p>
+        `
+        postsContainer.append(card)
+    });
+
+
+    
+
+    comment1.forEach((comment, i) => {
+        const card = document.createElement("div")
+        card.className = "cardComment"
+        card.innerHTML = `
+        <img src = "https://picsum.photos/200?random=${i}">
+        <p><stong>${comment.postId}</strong></p>
+        <p>${comment.name}</p>
+        <p>${comment.email}</p>
+        <p>${comment.body}<p>
+        `
+        commentsContainer.append(card)
+    });
+
+album1.forEach((album, i) => {
+    const card = document.createElement("div")
+    card.className = "cardAlbum"
     card.innerHTML = `
-      <img src="https://picsum.photos/200?random=${i}">
-      <p><strong>${user.name}</strong></p>
-      <p>${user.email}</p>
-      <p>${user.address.city}</p>
-    `;
-    usersContainer.append(card);
-  });
+    <img src = "https://picsum.photos/200?random=${i}">
+    <p><strong>${album.userId}</strong></p>
+    <p>${album.title}</p>
+
+    `
+    albumsContainer.append(card)
+});
 
 
-
-  todos.forEach((todo, i) => {
-    const card = document.createElement("div");
-    card.className = "cardtodo";
+photo1.forEach((photo, i) => {
+    const card = document.createElement("div")
+    card.className = "cardPhoto"
     card.innerHTML = `
-      <img src="https://picsum.photos/200?random=${i}">
-      <p><strong>${todo.title}</strong></p>
-    `;
-    todosContainer.append(card);
-  });
+        <img src = "https://picsum.photos/200?random=${i}">
+        <p><strong>${photo.albumId}</strong></p>
+        <p>${photo.title}</p>
+        <p>${photo.url}</p>
+        <p>${photo.thumbnailUrl}</p>
+    `
+    photosContainer.append(card)
+});
 
-  albums.forEach((album, i) => {
-    const card = document.createElement("div");
-    card.className = "cardalbum";
+
+todo1.forEach((todo, i )=> {
+    const card = document.createElement("div")
+    card.className = "cardTodo"
     card.innerHTML = `
-      <img src="https://picsum.photos/200?random=${i}">
-      <p><strong>Album:</strong> ${album.title}</p>
-      <p>User ID: ${album.userId}</p>
-    `;
-    albumsContainer.append(card);
-  });
+    <img src = "https://picsum.photos/200?random=${i}">
+    <p><strong>${todo.userId}</strong></p>
+    <p>${todo.title}</p>
+    <p>${todo.completed}</p>
+    `
+    todosContainer.append(card)
+});
+
+
+user1.forEach((user, i) => {
+const card = document.createElement("div") 
+card.className = "cardUser"
+card.innerHTML = `
+<img src = "https://picsum.photos/200?random=${i}">
+<p><strong>${user.id}</strong></p>
+<p>${user.name}</p>
+<p>${user.username}</p>
+<p>${user.email}</p>
+<p>${user.address.city}</p>
+`   
+usersContainer.append(card)
+});
+
+
+
 }
 
-myDatas();
+
+myFunc()
+
+
+
